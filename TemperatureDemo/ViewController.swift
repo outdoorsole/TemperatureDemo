0943571ea15462841d0259431aa0ff02eec10c25
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-11-21 19:55:25 -0800

    Extrapolate extra code in VC

--- TemperatureDemo/ViewController.swift
+++ TemperatureDemo/ViewController.swift
@@ -10,8 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
-//    var temps = [10, 12, 15, 18, 20, 21, 25, 26, 31, 32, 33, 34, 35, 36, 37, 38, 39]
-    var temps = [Int]()
+    var tempModel = TemperatureModel()
     
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
@@ -20,19 +19,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        for i in -50...150 {
-            temps.append(i)
-        }
-        
-        tempLabel.text = ("\(toFahrenheit(celsius: temps[0])) °F")
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return temps.count
+        tempLabel.text = ("\(tempModel.toFahrenheit(celsius: tempRange.temps[0])) °F")
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
@@ -44,18 +31,15 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         print("didSelectRow \(row) for temp \(temps[row]) C, \(toFahrenheit(celsius: temps[row])) F")
         tempLabel.text = ("\(toFahrenheit(celsius: tempC)) °F")
         
-        if tempC < 0 {
-            imageView.image = UIImage(named: "ice")
-        } else if tempC <= 100 {
-            imageView.image = UIImage(named: "water")
-        } else {
-            imageView.image = UIImage(named: "steam")
-        }
+//        if tempC < 0 {
+//            imageView.image = UIImage(named: "ice")
+//        } else if tempC <= 100 {
+//            imageView.image = UIImage(named: "water")
+//        } else {
+//            imageView.image = UIImage(named: "steam")
+//        }
         
     }
-    
-    func toFahrenheit(celsius: Int) -> String {
-        return String(Double(celsius) * 1.8 + 32)
-    }
+
 }
 
